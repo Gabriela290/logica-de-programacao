21913fa4f8cfbdd515f82a7fbb0c61addcf80e28
Gabriela290 <gabriela.amaral.fumcad@despertar.org.br>
2023-03-08 11:25:55 -0300

    AulaUm

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+var numeroUm = 0;
+let numeroDois = 20;
+var numeroTres = 40;
+
+console.log(numeroUm)
+
+numeroUm = numeroDois;
+console.log(numeroUm)
+
+numeroUm = (numeroTres);
+console.log(numeroUm);
+

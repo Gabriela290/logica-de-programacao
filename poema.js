c65ccdf7fc994c0dc42d5ea16229bad1e5f38365
Gabriela290 <gabriela.amaral.fumcad@despertar.org.br>
2023-03-10 09:24:59 -0300

    M

---
+++ poema.js
@@ -0,0 +1,16 @@
+let temDinhero = true;
+let temSol = false;
+
+if (temDinhero) {
+  if (temSol) {
+ console.log( "Guarajá" );
+  } else {
+ console.log("Restaurante");
+  }
+ } else {
+    if (temSol) {
+ console.log("Ibirapuera");
+    } else {
+        console.log("Aula");
+    }
+}
